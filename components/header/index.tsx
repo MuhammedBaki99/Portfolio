281472d4dd06f985a6f39ed77ed5956e986af8ed
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2025-09-05 11:31:49 +0300

    Enhance global styles with custom scrollbar, update About section layout, and implement responsive design in Header and Hero components. Introduce BubbleMenu for mobile navigation and refine text styles for better readability.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,8 +1,9 @@
 "use client"
- 
+
 import GooeyNav from "../reactbits/gooeynav";
 import { useRef, useEffect } from "react";
 import gsap from "gsap";
+import BubbleMenu from "../reactbits/bubblemenu";
 
 
 export default function Header() {
@@ -30,21 +31,65 @@ export default function Header() {
     }
   }, []);
 
+  const bubblemenuitems = [
+    {
+      label: "Anasayfa",
+      href: "#anasayfa",
+      ariaLabel: "Anasayfa",
+      rotation: -8,
+      hoverStyles: { bgColor: "#3b82f6", textColor: "#ffffff" }
+    },
+    {
+      label: "Hakkımda",
+      href: "#hakkımda",
+      ariaLabel: "Hakkımda",
+      rotation: 8,
+      hoverStyles: { bgColor: "#10b981", textColor: "#ffffff" }
+    },
+    {
+      label: "Yetenekler",
+      href: "#skills",
+      ariaLabel: "Yetenekler",
+      rotation: 8,
+      hoverStyles: { bgColor: "#f59e0b", textColor: "#ffffff" }
+    },
+    {
+      label: "Projelerim",
+      href: "#",
+      ariaLabel: "Projelerim",
+      rotation: -8,
+      hoverStyles: { bgColor: "#8b5cf6", textColor: "#ffffff" }
+    }
+  ];
   return (
-    <div
-      ref={headerRef}
-      className="w-full flex justify-between items-center gap-20 p-2 fixed top-0 bg-card z-99"
-    >
-      <GooeyNav
-        items={items}
-        particleCount={15}
-        particleDistances={[90, 10]}
-        particleR={100}
-        initialActiveIndex={0}
-        animationTime={600}
-        timeVariance={300}
-        colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-      />
-    </div>
+    <> 
+      <div
+        ref={headerRef}
+        className="hidden md:flex w-full justify-between items-center gap-20 p-2 fixed top-0 bg-card z-99"
+      >
+        <GooeyNav
+          items={items}
+          particleCount={15}
+          particleDistances={[90, 10]}
+          particleR={100}
+          initialActiveIndex={0}
+          animationTime={600}
+          timeVariance={300}
+          colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+        />
+      </div>
+      <div className="md:hidden sticky top-0 z-99">
+        <BubbleMenu
+          items={bubblemenuitems}
+          menuAriaLabel="Toggle navigation"
+          menuBg="#ffffff"
+          menuContentColor="#111111"
+          useFixedPosition={false}
+          animationEase="back.out(1.5)"
+          animationDuration={0.5}
+          staggerDelay={0.12}
+        />
+      </div>
+    </>
   );
 }
